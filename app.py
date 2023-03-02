3a5e40795e4c71563eebac043dcee3f75c120bc0
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 20:54:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ shapefile = gpd.read_file("clipped-to-calgary.shp")
 # center on Liberty Bell, add marker
 m = folium.Map(tiles='OpenStreetMap',zoom_start=160)
 folium.Marker(
-     [51.0447, 114.0719]
+     [114.0719, 51.0447]
 ).add_to(m)
 folium.GeoJson(shapefile).add_to(m)
 # call to render Folium map in Streamlit
