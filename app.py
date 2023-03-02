f7ba9e0d811de8c7877c1c5b21899350a4ecc507
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 20:02:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ shapefile = gpd.read_file("clipped-to-calgary.shp",SHAPE_RESTORE_SHX = 'YES')
 map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
     
 choropleth = folium.Choropleth(
-        geo_data='clipped-to-calgary.shp',
-        #data=df,
-        #columns=('State Name', 'State Total Reports Quarter'),
-        key_on='feature.properties.name',
-        line_opacity=0.8,
-        highlight=True
+        geo_data='clipped-to-calgary.shp'
 )
 choropleth.geojson.add_to(map)
