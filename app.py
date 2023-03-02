bd8ff95e7cf3d764389e7be00bd25f9d6af1e9e8
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 19:32:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+import streamlit as st
+import pandas as pd
+import json
+from datetime import date
+from urllib.request import urlopen
+import time
+import altair as alt
+import folium
+import geopandas as gpd
+
 shapefile = gpd.read_file("clipped-to-calgary.shp",SHAPE_RESTORE_SHX = 'YES')
 print(shapefile)
