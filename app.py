09637cbc0146395e4bfad4c23945194905d46469
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 20:24:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import geopandas as gpd
 
 shapefile = gpd.read_file("clipped-to-calgary.shp",SHAPE_RESTORE_SHX = 'YES')
 
-map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
-    
 choropleth = folium.Choropleth(
-        geo_data='clipped-to-calgary.shp'
-)
+        geo_data='data/us-state-boundaries.geojson',
+        data=shapefile,
+        columns=('cfsauid'),
+        key_on='feature.properties.name',
+        line_opacity=0.8,
+        highlight=True
+    )
 choropleth.geojson.add_to(map)
