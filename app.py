5187c5d50703bc2f6a66dbd1850155b77b78a368
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 19:42:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,19 @@ import time
 import altair as alt
 import folium
 import geopandas as gpd
+import leafmap.foliumap as leafmap
 
 shapefile = gpd.read_file("clipped-to-calgary.shp",SHAPE_RESTORE_SHX = 'YES')
 print(shapefile)
 
-data = [1,2]
-print(data)
+map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
+    
+choropleth = folium.Choropleth(
+        geo_data='clipped-to-calgary.shp',
+        #data=df,
+        #columns=('State Name', 'State Total Reports Quarter'),
+        key_on='feature.properties.name',
+        line_opacity=0.8,
+        highlight=True
+)
+choropleth.geojson.add_to(map)
