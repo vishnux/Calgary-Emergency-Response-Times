b99e1ff233ff51c2c50199501939b6a2a26f93d4
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 22:12:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import geopandas as gpd
 
 # Load the shapefile using geopandas
 
-#df_fire = pd.read_excel("Fire_Stations_wcoordinates.xlsx")
+df_fire = pd.read_excel("Fire_Stations_wcoordinates.xlsx")
 shapefile = gpd.read_file("clipped-to-calgary.shp")
 
 m = shapefile.explore()
-#for idx, row in df_fire.iterrows():
-#    folium.Marker(location=[row["LAT"], row["LON"]], icon=folium.Icon(icon="circle", prefix='fa', color='blue')).add_to(m)
+for idx, row in df_fire.iterrows():
+    folium.Marker(location=[row["LAT"], row["LON"]]).add_to(m)
 st_data = st_folium(m, width=725)
