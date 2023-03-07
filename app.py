0545a723d0fafc94bd0221b911d9144633e8802e
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-06 20:37:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -55,14 +55,14 @@ shapefile = gpd.read_file("clipped-to-calgary.shp")
 # Create a map
 m = folium.Map(location=[51.0447,-114.0719], zoom_start=11)
 
-#Add the fire stations as markers with popups
-for index, row in df_fire.iterrows():
-    folium.Marker(
-        location=[row["LAT"], row["LON"]],
-        popup=row["NAME"],
-        tooltip=row["NAME"],
-        icon=folium.Icon(color="red")
-    ).add_to(m)
+# #Add the fire stations as markers with popups
+# for index, row in df_fire.iterrows():
+#     folium.Marker(
+#         location=[row["LAT"], row["LON"]],
+#         popup=row["NAME"],
+#         tooltip=row["NAME"],
+#         icon=folium.Icon(color="red")
+#     ).add_to(m)
     
 # for index, row in df_fire.iterrows():
 #     folium.CircleMarker(
@@ -78,6 +78,24 @@ for index, row in df_fire.iterrows():
 #         opacity=1,
 #         weight=1
 #     ).add_to(m)
+
+# Add the fire stations as circle markers with popups and labels
+for index, row in df_fire.iterrows():
+    folium.CircleMarker(
+        location=[row["LAT"], row["LON"]],
+        popup=row["NAME"],
+        tooltip=row["NAME"],
+        radius=2,
+        fill=True,
+        fill_color="red",
+        fill_opacity=1,
+        color="red",
+        opacity=1,
+        weight=1,
+        # Add a label to the circle marker
+        tooltip=folium.Popup(row["NAME"], parse_html=True)
+    ).add_to(m)
+
 
 # Add the shapefile to the map
 folium.GeoJson(
