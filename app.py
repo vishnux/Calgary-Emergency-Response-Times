74cd53d1cbffccbfc96599752b8aed4e34f4248c
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-29 19:34:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,18 @@ if fsa_search:
             icon=folium.Icon(color="green", icon="info-sign"),
         ).add_to(m)
 
-# Render the map in Streamlit
-st_data = st_folium(m, width=725, height=550)
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.write(' ')
+
+with col2:
+    # Render the map in Streamlit
+    st_data = st_folium(m, width=725, height=550)
+
+with col3:
+    st.write(' ')        
+        
 
 #TEST 
 
