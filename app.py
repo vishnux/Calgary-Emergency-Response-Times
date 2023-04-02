77f414f23b363ffc1460793de074d9a8e2d51a43
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-01 23:05:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -132,18 +132,18 @@ with col3:
 
     
 # with st.expander("What is an FSA?"):
-#     st.write(\"\"\"
+#     st.write("""
 #         A forward sortation area (FSA) is a way to designate a geographical unit based on the first three characters 
 #              in a Canadian postal code. All postal codes that start with the same three characters—for example, 
 #              K1A—are together considered an FSA.
 #     \"\"\")    
 
 with st.expander("See explanation"):
-    st.write(\"\"\"
+    st.write("""
         The chart above shows some numbers I picked for you.
         I rolled actual dice for these, so they're *guaranteed* to
         be random.
-    \"\"\")
+    """)
     
 # # Show a bar chart of the average response times by FSA
 # fig = px.bar(df_avgtime_fire, x='FSA', y='Avg_time', labels={'FSA':'Forward Sortation Area', 'Avg_time':'Average Response Time (mins)'})
