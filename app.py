f6ad9eb5d4141ce3e9b33cd4b1526c8f9da2c2a9
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-02 11:03:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ shapefile = gpd.read_file("clipped-to-calgary.shp")
 df_avgtimes_fire = shapefile.merge(df_avgtime_fire, left_on="cfsauid", right_on="FSA")
 df_avgtimes_fire["Avg_time"] = df_avgtimes_fire["Avg_time"].round(2)
 # Sort data by highest response time per FSA to lowest
-df_avgtime_fire = df_avgtime_fire.sort_values(by=['Avg_time'], ascending=[False])
+df_avgtimes_fire = df_avgtime_fires.sort_values(by=['Avg_time'], ascending=[False])
 # Define color scale
 color_scale = folium.LinearColormap(
     colors=["blue", "green", "yellow", "red"],
@@ -68,17 +68,17 @@ folium.GeoJson(
 color_scale.add_to(m)
 folium.LayerControl().add_to(m)
 
-# Search Bar for FSA
-fsa_search = st.sidebar.text_input("Search for FSA:")
-if fsa_search:
-    fsa_data = df_avgtime_fire[df_avgtime_fire["FSA"].str.contains(fsa_search)]
-    for index, row in fsa_data.iterrows():
-        folium.Marker(
-            location=[row["LAT"], row["LON"]],
-            popup=row["NAME"],
-            tooltip=row["NAME"],
-            icon=folium.Icon(color="green", icon="info-sign"),
-        ).add_to(m)
+# # Search Bar for FSA
+# fsa_search = st.sidebar.text_input("Search for FSA:")
+# if fsa_search:
+#     fsa_data = df_avgtime_fire[df_avgtime_fire["FSA"].str.contains(fsa_search)]
+#     for index, row in fsa_data.iterrows():
+#         folium.Marker(
+#             location=[row["LAT"], row["LON"]],
+#             popup=row["NAME"],
+#             tooltip=row["NAME"],
+#             icon=folium.Icon(color="green", icon="info-sign"),
+#         ).add_to(m)
 
 col1, col2,col3 = st.columns((0.5,1,0.5))#gap="large"
 
@@ -102,20 +102,12 @@ with col1:
      # Show a bar chart of the average response times by FSA
      st.write("## Average Response Time by Forward Sortation Area (FSA)")
      st.write("The following bar chart shows the average response times by Forward Sortation Area (FSA) in minutes. You can hover over the bars to see the exact values.")
-     fig_bar = px.bar(df_avgtime_fire, x='FSA', y='Avg_time', labels={'FSA':'Forward Sortation Area', 'Avg_time':'Average Response Time (mins)'})
+     fig_bar = px.bar(df_avgtimes_fire, x='FSA', y='Avg_time', labels={'FSA':'Forward Sortation Area', 'Avg_time':'Average Response Time (mins)'})
      fig_bar.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
      fig_bar.update_layout(title_text='Average Emergency Response Times by FSA')
      fig_bar.add_hline(y=6, line_dash="dash", line_color="red",
               annotation_text="Target",annotation_font_color="red")
      st.plotly_chart(fig_bar, use_container_width=True)
-      # Show a bar chart of the average response times by FSA
-#         st.write("## Average Response Time by Forward Sortation Area (FSA)")
-#         st.write("The following bar chart shows the average response times by Forward Sortation Area (FSA) in minutes. You can hover over the bars to see the exact values.")
-#         fig_bar = px.bar(df_avgtime_fire, x='FSA', y='Avg_time', labels={'FSA':'Forward Sortation Area', 'Avg_time':'Average Response Time (mins)'})
-#         fig_bar.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
-#         fig_bar.update_layout(title_text='Average Emergency Response Times by FSA')
-        
-#         st.plotly_chart(fig_bar, use_container_width=True)
 
 
 with col2:
@@ -130,7 +122,7 @@ with col3:
     \n
     The following histogram shows the distribution of response times in minutes. ''')
     #bins = st.slider("Select the number of bins", min_value=5, max_value=50, value=20)
-    fig_hist = px.histogram(df_avgtime_fire, x='Avg_time', nbins=20, labels={'Avg_time':'Average Response Time (mins)'})
+    fig_hist = px.histogram(df_avgtimes_fire, x='Avg_time', nbins=20, labels={'Avg_time':'Average Response Time (mins)'})
     fig_hist.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
     fig_hist.update_layout(title_text='Distribution of Emergency Response Times',yaxis_title = 'Count')
     fig_hist.add_vline(x=6, line_dash="dash", line_color="red",
