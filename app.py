2d2d63eab69acf97f0e604bb1fe50fb767bcf193
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-01 18:01:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with col1:
 
 with col2:
     # Render the map in Streamlit        
-    st_data = st_folium(m, width=725, height=550)
+    st_data = st_folium(m, width=825, height=650)
     
 with col3:
     # Add some vertical space between the graphs
