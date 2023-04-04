3c12c0dec1021e8e7053408210e9e9c2a5238f41
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-03 19:40:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.sidebar.markdown("# Main page 🎈")
 # Contents of ~/my_app/pages/page_2.py
 
 #st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+#st.sidebar.markdown("# Page 2 ❄️")
 
 # Contents of ~/my_app/pages/page_3.py
 
