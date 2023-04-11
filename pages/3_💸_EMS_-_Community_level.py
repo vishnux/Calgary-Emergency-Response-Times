50412d976d7f61c59e7d70ebca02f2a4dea23de0
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-10 19:02:44 -0600

    Update 3_💸_EMS_-_Community_level.py

--- pages/3_💸_EMS_-_Community_level.py
+++ pages/3_💸_EMS_-_Community_level.py
@@ -103,7 +103,7 @@ with col1:
      # Show a bar chart of the average response times by Community
      st.write("## Average Response Time by Community")
      st.write(''' 
-     In the event of an emergency, **Glacier Ridge**, **Legacy**, **Hotchkiss** and **Wolf Willow* are the communities which aren't residual sub areas with the lowest coverage, potentially leaving those areas vulnerable and in need of 
+     In the event of an emergency, **Glacier Ridge**, **Legacy**, **Hotchkiss** and **Wolf Willow** are the communities which aren't residual sub areas with the lowest coverage, potentially leaving those areas vulnerable and in need of 
      additional support. \n
      The following bar chart shows the average response times by Community in minutes. ''')
      fig_bar = px.bar(df_avgtimes_ems, x='Community', y='Avg_time', labels={'Community':'Community', 'Avg_time':'Average Response Time (mins)'})
