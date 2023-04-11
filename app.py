23ea59ef2d8ec76ddb8796d24fff660e6b8de838
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-10 19:38:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ show_pages(
         Page("app.py", "Home", "🏠"),
         Page("pages/1_Fire_Station_-_FSA_level.py", "Fire_Station_-_FSA_level","🌍"),
         Page("pages/2_Fire_Station_-_Community_level.py", "Fire_Station_-_Community_level","🛒"),
-        Page("pages/3_🪙_EMS_-_FSA_level.py", "EMS_-_FSA_level","🪙"),
-        Page("pages/4_💸_EMS_-_Community_level.py", "EMS_-_Community_level","💸"),
+        Page("pages/3_EMS_-_FSA_level.py", "EMS_-_FSA_level","🪙"),
+        Page("pages/4_EMS_-_Community_level.py", "EMS_-_Community_level","💸"),
     ]
 )
