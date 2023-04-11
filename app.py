01be18a95d68790313dd29fc8114a5b9370e14b1
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-04-10 19:29:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-import streamlit as st
+from st_pages import Page, show_pages, add_page_title
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
+# Optional -- adds the title and icon to the current page
+add_page_title()
 
-st.write("# Welcome to Streamlit! 👋")
-
-st.sidebar.success("Select a demo above.")
-
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+show_pages(
+    [
+        Page("app.py", "Home", "🏠"),
+     
+    ]
 )
