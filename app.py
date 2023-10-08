d6419a6167f8309e62778a9206903a776567beda
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-10-08 09:55:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
+# Define a SessionState class to store the selected page
+class SessionState:
+    def __init__(self):
+        self.page = "app.py"
+
+# Create an instance of SessionState
+session_state = SessionState()
+
 # Optional -- adds the title and icon to the current page
 # add_page_title()
 
@@ -18,7 +26,7 @@ show_pages(
 )
 
 # Define the content of the executive summary page (app.py)
-if st.session_state.page == "app.py":
+if session_state.page == "app.py":
     st.title("Calgary Emergency Response Time Analysis")
     st.write("## Introduction")
     st.write("The Calgary Emergency Response Time Analysis project undertakes a comprehensive analysis of the response times exhibited by Emergency Services in the city of Calgary. The primary objective of this research is to gain a deeper understanding of the efficiency and effectiveness of emergency responses, which are critical factors in ensuring public safety and well-being.")
@@ -27,6 +35,3 @@ if st.session_state.page == "app.py":
     st.write("It is essential to underscore the importance of this research as it directly impacts public safety and has the potential to save lives. By critically evaluating and improving emergency response times, we strive to create a safer and more resilient community for all residents of Calgary.")
     st.write("We acknowledge the limitations of the study, including potential data constraints and external factors that may influence response times.")
     st.write("In conclusion, this research endeavors to contribute valuable insights into the realm of Emergency Services response times in Calgary. By shedding light on the efficiency and effectiveness of emergency responses, we seek to foster a culture of continuous improvement in emergency service delivery, ultimately benefiting the citizens and ensuring the city's preparedness for unforeseen emergencies.")
-
-
-
